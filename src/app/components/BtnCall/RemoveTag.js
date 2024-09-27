f2c08087d7e7ac3352c99e54c727a03495c9b303
tumi267 <itu.matlala4@gmail.com>
2024-09-27 13:00:02 +0200

    remove tag flow

--- src/app/components/BtnCall/RemoveTag.js
+++ src/app/components/BtnCall/RemoveTag.js
@@ -1,9 +1,20 @@
 'use client'
 
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import styles from './card.module.css'
+import getAudience from "@/app/libs/getAudience"
+import getAudlist from "@/app/libs/getAudlist"
 function RemoveTag() {
     const [details,setDetails]=useState({name:'',email:'',tagName:''})
+    const [lists, setLists] = useState([]);
+    const [members,setmembers]=useState([])
+    const [selectedMemberIndex, setSelectedTab] = useState(0);
+    useEffect(()=>{
+      getAudience(setLists)
+    },[])
+    const handleMemberSelection=(index,ele)=>{
+      setSelectedTab(index);
+    }
     const handleSubmit = async (event) => {
         event.preventDefault()
         // Handle the submission of the form with the audience data
@@ -15,20 +26,37 @@ function RemoveTag() {
         const msg=await res.json()
         alert(msg.msg)
       }
+
   return (
     <div className={styles.contain}>
       <br/>
       <h3>Remove Tag</h3> 
       <br/>
-        <form onSubmit={handleSubmit}>
-            <input className={styles.inputBar} type="text" value={details.name} placeholder="audiance name" onChange={(e)=>{setDetails({...details,name:e.target.value})}}/>
-
-            <input className={styles.inputBar} type="text" value={details.tagName} placeholder="tag name" onChange={(e)=>{setDetails({...details,tagName:e.target.value})}}/>
-
-            <input className={styles.inputBar} type="email" value={details.email} placeholder="audiance member email" onChange={(e)=>{setDetails({...details,email:e.target.value})}}/>
-            <br/>
-            <button className={styles.btn} type="submit">submit</button>
-        </form>
+      <hr/>
+      {lists.map((e,i)=>{return <div key={i} onClick={()=>{getAudlist(e,setmembers), setDetails({...details,name:e.name})}}>{e.name}</div>})}
+      <input className={styles.inputBar} type="text" value={details.tagName} placeholder="tag name" onChange={(e)=>{setDetails({...details,tagName:e.target.value})}}/> 
+      <table >
+        <thead>
+        <tr>
+        {members.length>0&&<th>name</th>}
+        {members.length>0&&<th>email</th>}
+        {members.length>0&&<th>tags</th>}
+        </tr>
+        </thead>
+        <tbody>
+        {members.length>0&&members.map((e,i)=>{return <tr key={i} onClick={()=>{setDetails({...details,email:e.email_address})}}>
+        <td>
+        <input type='radio' value={e.full_name}
+        checked={selectedMemberIndex === i}
+        onChange={() => handleMemberSelection(i,e)}/>
+        {e.full_name}</td>
+        <td>{e.email_address}</td>
+        <td>{e?.tags.map((j,y)=>{return <p key={y}>{j.name}</p>})}</td>
+        </tr>
+        })}
+         </tbody>
+       </table>  
+       <button className={styles.btn} onClick={handleSubmit}>submit</button>
     </div>
   )
 }
